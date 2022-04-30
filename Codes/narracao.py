e1173dab68d91de4ca72b52bdcc7d32eaa7db131
Moura <hiroganja@gmail.com>
2022-04-30 10:26:40 -0300

    att codiguin :)

--- Codes/narracao.py
+++ Codes/narracao.py
@@ -3,6 +3,11 @@ from time import sleep
 escolhaidade = 0
 acao01 = 0
 acao02 = 0
+acao03 = 0
+
+# iteminicial
+# carta
+# relacao
 
 print('***'*21)
 print('                  Jovem Filho do Sol PRESENTS')
@@ -76,21 +81,21 @@ dar ouvidos aos místicos excêntricos que aparecem em seus conturbados auges, o
 que de alguma forma te fez detestar cada vez mais os que não eram céticos.''')
 sleep(10)
 print(' ')
-print(''' Era notável que o jovem {} não gostava muito da ideia de compromissos
+print(''' Era notável que o jovem {} não gosta muito da ideia de compromissos
 sociais, sobretudo por estarem em um dia que o deixava exausto, sempre. Obviamente
 a presença dos Cultuadores e do Culto em si deixavam tudo ainda pior.'''. format(nome))
 sleep(11)
 print(' ')
-print(''' Era uma manhã chuvosa e fria, com ventos cortantes e barulhentos.
+print(''' É uma manhã chuvosa e fria, com ventos cortantes e barulhentos.
 Atrás da grande porta de cedro escuro, ouvia-se apenas o caminhar apressado
 de sapatos femininos como quem tentava correr de forma discreta. Após alguns
 segundos, o som estrindente dos sapatos foi se aproximando, até que a grande
 porta de cedro foi sendo afastada aos poucos.''')
 sleep(13)
 print(' ')
-print(''' Quando aberta, a passagem recebeu uma moça jovem que tropeçava nos
-próprios pés enquanto tentava fechar a porta atrás de si. Ela se atrapalhava e
-tentava a todo custo disfarçar sua falta de experiência, por mais que fizesse com
+print(''' Quando aberta, a passagem recebe uma moça jovem que tropeça nos
+próprios pés enquanto tenta fechar a porta atrás de si. Ela se atrapalha e
+tentav a todo custo disfarçar sua falta de experiência, por mais que fizesse com
 boa vontade. Tendo conseguido encostar a pesada porta, a jovem olhou deslumbrada
 para o que conhecerá: ela estava lá, os aposentos do Jovem Filho do Sol.''')
 sleep(13)
@@ -136,7 +141,7 @@ Quando ela te viu acordar, um sorriso de alívio foi tão presente que quase se 
 de esconder.''')
 print(' ')
 sleep(10)
-print(''' Acordando desorientado devido ao susto, o jovem olha para cima e vê a figura
+print(''' Acordando desorientado devido ao susto, o Jovem olha para cima e vê a figura
 reconfortante de uma garota que aparentava ter sua idade. Ela, ao contrário das outras
 que já passaram por ali, não parecia ter receio de tocá-lo, e ele praticamente estava
 deitado em seus braços enquanto processava tudo.''')
@@ -151,9 +156,9 @@ aceitável se estivessemos falando de um tempo maior do que dez dias, o que
 não era o caso.''')
 print(' ')
 sleep(4)
-print(''' Você claramente não conhecia a mulher e ficara constrangido com isso. Ao
-contrário do resto da família, você não se sentia mais especial do que os que trabalhavam
-na casa. Para falar a verdade, o Jovem Filho do Sol tinha consideráveis problemas
+print(''' Você claramente não conhece a mulher e ficara constrangido com isso. Ao
+contrário do resto da família, você não se sente mais especial do que os que trabalhavam
+na casa. Para falar a verdade, o Jovem Filho do Sol tem consideráveis problemas
 de autoestima.''')
 print(' ')
 sleep(9)
@@ -168,6 +173,7 @@ provavelmente estaria ansiosa e com medo.''')
     acao01 = int(input('Olhando para a mulher a sua frente, o que você deseja fazer? '))
     print(' ')
     if acao01 == 1:
+        relacao = 'boa'
         print(''' {} pensa que, dada a pressão que a moça estaria sofrendo, esse estaria
 sendo um dia horrível para ela. Considerando sua criação, a empatia é até que grande
 em sua pessoa. Apesar de sua timidez te forçar a ficar em silêncio, você consegue acenar
@@ -178,12 +184,14 @@ mesma.
 já aguarda pelas instruções do ritual que deverá ser orientado por ela.'''.format(nome))
         sleep(12)
     elif acao01 == 2:
+        relacao = 'ruim'
         print(''' A ansiedade dela era inquietante de fato, coisa que chegava a te incomodar também.
 Você pensa em dizer algumas palavras para confortá-la, mas a timidez lhe impede. Depois de alguns
 segundos em uma troca de olhares desconfortável, {} simplesmente levanta e se coloca em pé ao
 lado da cama, tudo conforme o ritual manda.'''.format(nome))
         sleep(9)
     elif acao01 == 3:
+        relacao = 'otima'
         print(''' Apesar da chuva e dos compromissos, {} tivera uma ótima noite de sono e estava de bom humor.
 Contemplou a situação da moça por breves segundos e rapidamente se sensibilizou com sua ansiedade
 e desajeito. Não se sabe a razão, mas aquela garota havia lhe despertado um sentimento de pertencimento,
@@ -220,7 +228,7 @@ ela começa a te olhar em completo silêncio. A moça claramente estava te desaf
 verdade é a razão pela qual estamos aqui, eu e Elas.''')
 print(' ')
 sleep(7)
-print(''' Elas? Elas quem? Isso nunca acontecera, você não faz ideia da razão pela qual está sendo sutilmente
+print(''' "Elas"? "Elas" quem? Isso nunca acontecera, você não faz ideia da razão pela qual está sendo sutilmente
 ameaçado por sua serviçal particular.''')
 print(' ')
 sleep(5)
@@ -243,14 +251,14 @@ com satisfação visível. Seja lá o que estava tramando, deu certo.''')
 print(' ')
 sleep(5)
 print(''' - {}, eu tenho novidades para você... -- caminhando até o centro do quarto, ela abre a bolsa de couro e despeja
-seu conteúdo sobre a mesa redonda de mármore. Você pode observar alguns itens curiosos, dentre eles um pote contendo um líquido
+seu conteúdo sobre a mesa redonda de mármore. Você pode observar alguns itens curiosos, dentre eles um frasco contendo um líquido
 rosa vibrante, algo que parecia ser uma bússola ou um relógio, uma pulseira verde-esmeralda da mesma tonalidade da adaga e uma
 corrente dourada cravejada de brilhantes. A moça pega apenas os dois últimos itens e caminha até você.'''.format(nome))
 print(' ')
 sleep(10)
 
 while acao02 != 1 and acao02 != 2:
-    print('''(ação.02) Opções de itens: 
+    print('''(ação.02) Opções de item: 
   [1] - Pulseira verde-esmeralda
   [2] - Corrente dourada cravejada de brilhantes  ''')
     print(' ')
@@ -268,3 +276,73 @@ print(' ')
 sleep(2)
 print(''' {} olha para ela com medo. Existe uma certa resistência, mas após alguns segundos o Jovem estende sua mão
 e pega a {} que ela guardava.'''.format(nome, iteminicial))
+print(' ')
+sleep(4)
+print(''' - Me desculpe pela péssima brincadeira, {}. Mas é que hoje será um dia diferente e eu acho que podemos
+fazer um trato bom para ambos os lados. Eu estou tão nervosa quanto você, pode ter certeza quanto a isso.'''.format(nome))
+print(' ')
+sleep(6)
+print(''' O Jovem a olha com uma dúvida latente. Não entende de fato sobre o que "um trato" significava.
+Estava com medo da moça, mas também intrigado para saber o que o dia de hoje reservava.''')
+print(' ')
+sleep(5)
+print(''' - Certo, podemos fechar um acordo. Mas antes preciso que me explique o que está acontecendo. Além disso,
+exijo saber o seu nome. Não confio em quem não responde por si. -- disse {} com a voz trêmula. Era notável sua falha
+tentativa de impor respeito, mas ele não era bom nisso.'''.format(nome))
+print(' ')
+sleep(8)
+print(''' A moça também estava ansiosa, mas agora sentia mais autoconfiança. Sabia que seria fácil obter vantagens
+por intermédio de {}, mas não sabia que seria tão fácil. Ela se aproximou do Jovem novamente, dessa vez trazendo
+o líquido rosa vibrante consigo.'''.format(nome))
+print(' ')
+sleep(8)
+print(''' - Tome isso, {}. -- disse entregando o frasco em suas mãos -- Os outros Jovens não terão informações privilegiadas
+como você teve, então espero que valorize e que se lembre de mim. Quando estiver adentrando a Masmorra, peça licença e
+beba todo o líquido, mas cuidado para não lhe verem fazendo isso, você sofreria desgraças.'''.format(nome))
+print(' ')
+sleep(7)
+print(''' Você começa a analisar o frasco com cuidado, dessa vez encarando o líquido e tentando se lembrar se já o
+vira em algum outro momento de sua vida.''')
+print(' ')
+sleep(4)
+print(''' {}, assim como a maior parte dos jovens ricos, tivera um excelente ensino de conhecimentos gerais. O Jovem
+colecionava aulas da matéria Química Celeste e Poções Terrenas, mas não se lembrou de nada que se comportasse como
+o líquido daquele frasco. Ele se movia sozinho e vibrava em diferentes tons de rosa. Começou a nascer um receio de ingerir
+aquilo, e talvez de fato o Jovem não fosse beber.'''.format(nome))
+print(' ')
+sleep(11)
+print(''' - Tenho condições. Se quiser que lhe ajude, seja lá como exigir, terá que me detalhar o que acontecerá
+no dia de hoje, quem são "Elas", o que faz esse líquido e também qual é o seu nome, pergunta da qual você fugiu algumas
+vezes.''')
+print(' ')
+sleep(8)
+print(''' - Tudo bem, eu respondo todas. -- ela caminhou até o centro do cômodo novamente, próximo a mesa. Ali retirou um
+monte com cartas roxas com detalhes dourados, mas cada uma com tonalidades e tamanhos diferentes. -- Venha até aqui, {},
+pegue uma carta e vamos treinar.'''.format(nome))
+print(' ')
+sleep(7)
+
+while acao03 != 1 and acao03 != 2 and acao03 != 3:
+    print('''(ação.03) Opções de carta: 
+  [1] - Carta pequena. Roxo claro quase lilás, com detalhes dourados nas bordas
+  [2] - Carta média. Roxo escuro perto do azul, com muitos detalhes dourados no centro
+  [3] - Carta grande. Roxo vibrante e ''')
+    print(' ')
+    acao03 = int(input('Qual carta você quer pegar? '))
+    if acao03 == 1:
+        carta = ''
+        vantagem = ''
+    elif acao03 == 2:
+        carta = ''
+        vantagem = ''
+    elif acao03 == 3:
+        carta = ''
+        vantagem = ''
+    else:
+        print(' ')
+        print('''!! OPÇÃO INVÁLIDA. POR FAVOR, REFAÇA !!''')
+        print(' ')
+
+print(''' ABC''')
+print(' ')
+sleep(8)
